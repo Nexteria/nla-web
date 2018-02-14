aac006d9b877510a44e0edc64fab9a70462039fb
Filip Vozár <vozar.filip@gmail.com>
2018-02-14 19:38:51 +0100

    fix production settings

--- nla/context_processors.py
+++ nla/context_processors.py
@@ -3,5 +3,12 @@ from django.conf import settings
 
 def insert_config(_):
     return {
-        'settings': settings.SETTINGS_IN_VIEWS,
+        'settings': {
+            'exponea_token': settings.EXPONEA_TOKEN,
+            'exponea_target': settings.EXPONEA_TARGET,
+            'registration_email': settings.REGISTRATION_PERSON_EMAIL,
+            'registration_name': settings.REGISTRATION_PERSON_NAME,
+            'registration_phone': settings.REGISTRATION_PERSON_PHONE,
+            'registration_year': settings.REGISTRATION_YEAR,
+        },
     }
--- nla/settings/default.py
+++ nla/settings/default.py
@@ -30,20 +30,11 @@ EXPONEA_TRACK_REGISTRATION = False
 REGISTRATION_PERSON_EMAIL = "betka@nexteria.sk"
 REGISTRATION_PERSON_NAME = "Betka"
 REGISTRATION_PERSON_PHONE = "+421 948 049 905"
-
+REGISTRATION_YEAR = '2018'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['nla.nexteria.sk', 'localhost', '127.0.0.1']
-
-SETTINGS_IN_VIEWS = {
-    'exponea_token': EXPONEA_TOKEN,
-    'exponea_target': EXPONEA_TARGET,
-    'registration_email': REGISTRATION_PERSON_EMAIL,
-    'registration_name': REGISTRATION_PERSON_NAME,
-    'registration_phone': REGISTRATION_PERSON_PHONE,
-    'registration_year': '2018',
-}
 
 
 # Application definition
